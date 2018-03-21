804fead04c45c083300068e3e4a62ac9ef983df9
Luca Ledda <luca.ledda.93@gmail.com>
2018-03-21 19:10:52 +0100

    rosserial test on arduino: hostPC as publisher and arduino as subscriber

---
+++ arduino/rosserial_test/ros_ws/devel/share/rosserial_arduino/cmake/rosserial_arduino-extras.cmake
@@ -0,0 +1,4 @@
+cmake_minimum_required(VERSION 2.8.3)
+
+set(ROSSERIAL_ARDUINO_TOOLCHAIN "/home/luca/pp-robot-2018/arduino/rosserial_test/ros_ws/src/rosserial/rosserial_arduino/arduino-cmake/cmake/ArduinoToolchain.cmake")
+
---
+++ arduino/rosserial_test/ros_ws/devel/share/rosserial_mbed/cmake/rosserial_mbed-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(rosserial_mbed_MESSAGE_FILES "/home/luca/pp-robot-2018/arduino/rosserial_test/ros_ws/src/rosserial/rosserial_mbed/msg/Adc.msg")
+set(rosserial_mbed_SERVICE_FILES "/home/luca/pp-robot-2018/arduino/rosserial_test/ros_ws/src/rosserial/rosserial_mbed/srv/Test.srv")
