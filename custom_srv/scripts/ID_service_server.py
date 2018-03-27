6c7258af51b27260bc25b57db9e9e53344a8251b
edospi20 <edospi20@gmail.com>
2018-03-27 16:09:23 +0200

    First Attempt to enable ROS communication across two machines. Service handshakes work properly. Other problems related to remotely access a virtual machine have been encountered

--- custom_srv/scripts/ID_service_server.py
+++ custom_srv/scripts/ID_service_server.py
@@ -3,14 +3,19 @@
 from custom_srv.srv import *
 import rospy
 
+Robot_ID = 0
+
 def handle_new_robot(req):
-    print "Received request"
-    return AddNewRobotResponse(1)
+	global Robot_ID
+	print "Request received from robot"
+	Robot_ID += 1
+	# The Robot ID is returned to the robot requiring it
+	return AddNewRobotResponse(Robot_ID)
 
 def add_new_robot_server():
-    rospy.init_node('new_robot_server')
-    s = rospy.Service('add_new_robot',AddNewRobot , handle_new_robot)
-    print "Ready to add new robot."
+    rospy.init_node('robots_server')
+    s = rospy.Service('add_new_robot', AddNewRobot , handle_new_robot)
+    print "Ready to add a new robot!"
     rospy.spin()
 
 if __name__ == "__main__":
