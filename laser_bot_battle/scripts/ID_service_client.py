4d8ae27afc19b4c7a58178c282b110927e9ff51a
edospi20 <edospi20@gmail.com>
2018-03-31 12:41:42 +0200

    First stable package for the robot application

---
+++ laser_bot_battle/scripts/ID_service_client.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import rospy
+from laser_bot_battle.srv import *
+
+def add_new_robot_client():
+    # Wait until the service is not activated in ID_service_server.py
+    rospy.wait_for_service('add_new_robot')
+    try:
+    	new_robot = rospy.ServiceProxy('add_new_robot', AddNewRobot)
+        # new_robot() makes the request to the server and the returned ID is saved
+    	robot_ID = new_robot()
+    	print ("New robot ID = %d"%robot_ID.ID)
+        # The command to generate the ros node with unique name and topics is composed here
+        # TO DO (modify topic names when the real topics will be defined)
+    	command = 'rosrun rosserial_python serial_node.py /dev/ttyACM0 __name:=Robot' + str(robot_ID.ID)
+    	command += ' chatter:=Robot' + str(robot_ID.ID) + '/chatter'
+    	command += ' toggle_led:=Robot' + str(robot_ID.ID) + '/toggle_led'
+        # The new ros node is created
+    	os.system(command)
+    except rospy.ServiceException, e:
+    	print "Service call failed: %s"%e
+
+if __name__ == "__main__":
+	add_new_robot_client()
---
+++ laser_bot_battle/scripts/ID_service_server.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from laser_bot_battle.srv import *
+import rospy
+
+Robot_ID = 0
+
+def handle_new_robot(req):
+	global Robot_ID
+	print "Request received from robot"
+	Robot_ID += 1
+	# The Robot ID is returned to the robot requiring it
+	return AddNewRobotResponse(Robot_ID)
+
+def add_new_robot_server():
+    rospy.init_node('robots_server')
+    s = rospy.Service('add_new_robot', AddNewRobot , handle_new_robot)
+    print "Ready to add a new robot!"
+    rospy.spin()
+
+if __name__ == "__main__":
+    add_new_robot_server()
