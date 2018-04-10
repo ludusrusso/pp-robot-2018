e89d07da2b2ed76e63e2aaefff27790b7af6d782
edospi20 <edospi20@gmail.com>
2018-04-10 12:56:25 +0200

    Introduced the library AccelStepper for the movement of the two stepper motors controlling the two wheels. To be tested on real hw

--- arduino/Arduino_Robot_Sketchbook/rosserial_node_arduino/rosserial_node_arduino.ino
+++ arduino/Arduino_Robot_Sketchbook/rosserial_node_arduino/rosserial_node_arduino.ino
@@ -1,50 +1,100 @@
-#include <Stepper.h>
+#include <AccelStepper.h>
 #include <ros.h>
 #include <std_msgs/Empty.h>
 #include <laser_bot_battle/Robot_msg.h>
 
-/*-----( Declare Constants, Pin Numbers )-----*/
-//---( Number of steps per revolution of INTERNAL motor in 4-step mode )---
-#define STEPS_PER_MOTOR_REVOLUTION 32   
-
-//---( Steps per OUTPUT SHAFT of gear reduction )---
-#define STEPS_PER_OUTPUT_REVOLUTION 32 * 64  //2048 
+// Steps the motor has to perform at each Robot_msg received
+#define STEPS_PER_MOVEMENT_FW 16
+#define STEPS_PER_MOVEMENT_BW -16
+#define STEPS_PER_MOVEMENT_CW 16
+#define STEPS_PER_MOVEMENT_CCW -16
+// Default constant speed
+#define STEPPER_SPEED 100
 
 // ROS Node
 ros::NodeHandle  nh;
 
+// Messages exchanged 
 laser_bot_battle::Robot_msg robot_msg;      // Msg containing commands for the robot (move + fire)  
 std_msgs::Empty hit;                        // Msg to notify the robot has been hit
 
+// The Robot publishes a response to notify if it has been hit or not
 ros::Publisher pub("response", &robot_msg);
+// The Robot receive a Robot_msg and actuate the stepper motor and/or to the infrared driver.
+ros::Subscriber<laser_bot_battle::Robot_msg> sub("command", &robot_cb);
 
-Stepper small_stepper(STEPS_PER_MOTOR_REVOLUTION, 8, 10, 9, 11);
+AccelStepper stepper_l(AccelStepper::HALF4WIRE, 8, 10, 9, 11);
+// To define pin numbers for second motor
+AccelStepper stepper_r(AccelStepper::HALF4WIRE, 8, 10, 9, 11);
 
-//CallBack function to a received msg
-void stepper_cb( const laser_bot_battle::Robot_msg& cmd_msg){
+//CallBack function to manage a received msg
+void robot_cb( const laser_bot_battle::Robot_msg& cmd_msg){    
   
-  digitalWrite(13, HIGH-digitalRead(13));   // blink the led
-  int Steps2Take  =  10; 
-  
-  //nh.loginfo("received speed"); 
-  
+  nh.loginfo("Cmd Received"); 
+
+  // Storing the incoming command
   robot_msg.linear_x = cmd_msg.linear_x;
-  pub.publish(&robot_msg);
-  //small_stepper.setSpeed(cmd_msg.data);
-  //small_stepper.step(Steps2Take); 
+  robot_msg.angular_z = cmd_msg.angular_z;
+  robot_msg.shoot = cmd_msg.shoot;
+
+  // Linear movement has priority over Angular one
+  switch(robot_msg.linear_x){
+    case  1 :
+      // Move Forward
+      stepper_l.move(STEPS_PER_MOVEMENT_FW);
+      stepper_r.move(STEPS_PER_MOVEMENT_FW);
+      break;
+    case -1 :
+      // Move Backward
+      stepper_l.move(STEPS_PER_MOVEMENT_BW);
+      stepper_r.move(STEPS_PER_MOVEMENT_BW);
+      break;          
+  }   
+
+  if(robot_msg.linear_x == 0){
+    switch(robot_msg.angular_z){
+      case  1 :
+        // Move Clockwise
+        stepper_l.move(STEPS_PER_MOVEMENT_CW);
+        stepper_r.move(STEPS_PER_MOVEMENT_CCW);
+        break;
+      case -1 :
+        // Move Counter Clockwise
+        stepper_l.move(STEPS_PER_MOVEMENT_CCW);
+        stepper_r.move(STEPS_PER_MOVEMENT_CW);
+        break;
+      default :
+        // Stop the robot if both linear_x and angular_z = 0
+        stepper_l.stop();
+    }
+  }
+  
+  //pub.publish(&robot_msg); 
 }
 
-ros::Subscriber<laser_bot_battle::Robot_msg> sub("command", &stepper_cb);
-
 void setup(){
+  stepper_l.setMaxSpeed(STEPPER_SPEED);
+  //stepper_l.moveTo(0);
+
+  stepper_r.setMaxSpeed(STEPPER_SPEED);
+  //stepper_r.moveTo(0);
+  
   pinMode(13, OUTPUT);
+  
   nh.initNode();
   nh.advertise(pub);
   nh.subscribe(sub);
-  }
+}
 
 void loop(){
   //pub.publish(&speed_msg);
   nh.spinOnce();
-  delay(50);
+  // Set speed to constant speed
+  stepper_l.setSpeed(STEPPER_SPEED);
+  stepper_r.setSpeed(STEPPER_SPEED);
+  // Move the motor until the target position previously by move is reached (1 step per iteration)
+  stepper_l.runSpeedToPosition();
+  stepper_r.runSpeedToPosition();
+  // The delay should be consistent with the rate of incoming Robot_msgs (greater)
+  //delay(50);
 }
